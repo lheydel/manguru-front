42f4714c90b1cbd5067894411a7a2e1210103ccd
lheydel <luc.heydel@gmail.com>
2019-07-04 17:50:59 +0200

    travis skip cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_LOGIN" --password-stdin 
-# docker build . -t --tag=manguru-front
-# docker tag manguru-front louec/manguru:front-$1
-# docker push louec/manguru:front-$1
+docker build . -t --tag=manguru-front
+docker tag manguru-front louec/manguru:front-$1
+docker push louec/manguru:front-$1
