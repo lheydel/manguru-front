da88ce0406f1f5e9c66551df7721a3fed6abb6fb
lheydel <luc.heydel@gmail.com>
2019-07-24 13:17:26 +0200

    fix AuthedRoute tests

--- src/components/AuthedRoute/__tests__/AuthedRoute.spec.tsx
+++ src/components/AuthedRoute/__tests__/AuthedRoute.spec.tsx
@@ -2,13 +2,18 @@ import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
-import { Cookies } from 'react-cookie';
+import { Cookies, CookiesProvider } from 'react-cookie';
 import { Provider } from 'react-redux';
 import { Redirect, Route, Router } from 'react-router';
+import { Store } from 'redux';
 import createMockStore from 'redux-mock-store';
 import AuthRoute, { AuthedRouteProps, RawAuthedRoute } from '..';
-import { initialAppState } from '../../../services/common/app.state';
+import { User } from '../../../models/user.model';
+import { ApplicationState, initialAppState } from '../../../services/common/app.state';
+import userActionners from '../../../services/user/actions/user.actionners';
 import { initialUserLoginState } from '../../../services/user/reducers/user.states';
+import userService from '../../../services/user/service/user.service';
+import { Cookie } from '../../../utils/properties';
 
 describe('AuthedRoute component', () => {
     const routeProps: AuthedRouteProps = {
@@ -19,11 +24,15 @@ describe('AuthedRoute component', () => {
         loginState: initialUserLoginState,
     };
 
-    const routerWrapper = (props: AuthedRouteProps) => {
+    const connectedWrapper = (props: AuthedRouteProps, store: Store) => {
         return mount(
-            <Router history={createBrowserHistory()}>
-                <RawAuthedRoute {...props}></RawAuthedRoute>
-            </Router>
+            <Provider store={store}>
+                <CookiesProvider cookies={props.cookies}>
+                    <Router history={createBrowserHistory()}>
+                        <AuthRoute {...props}></AuthRoute>
+                    </Router>
+                </CookiesProvider>
+            </Provider>
         );
     };
 
@@ -33,25 +42,55 @@ describe('AuthedRoute component', () => {
     });
 
     it('should render itself and children when connected', () => {
-        const wrapper = mount(
-            <Provider store={createMockStore()(initialAppState)}>
-                <Router history={createBrowserHistory()}>
-                    <AuthRoute {...routeProps}></AuthRoute>
-                </Router>
-            </Provider>
-        );
-        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(toJson(connectedWrapper(routeProps, createMockStore()(initialAppState)))).toMatchSnapshot();
     });
 
     it('should render the component given in props when user is logged', () => {
-        routeProps.loginState = { ...routeProps.loginState, logged: true };
-        const wrapper = routerWrapper(routeProps);
+        const state: ApplicationState = {
+            ...initialAppState,
+            user: {
+                login: {
+                    logged: true,
+                    loading: false,
+                }
+            }
+        };
+        const wrapper = connectedWrapper(routeProps, createMockStore()(state));
         expect(wrapper.find(Route).props().render()).toEqual(<routeProps.component />);
     });
 
-    it('should redirect when user is not logged', () => {
-        routeProps.loginState = { ...routeProps.loginState, logged: false };
-        const wrapper = routerWrapper(routeProps);
+    it('should redirect when user is and can not logged', () => {
+        const state: ApplicationState = {
+            ...initialAppState,
+            user: {
+                login: {
+                    logged: false,
+                    loading: false,
+                    error: 'err',
+                }
+            }
+        };
+        const wrapper = connectedWrapper(routeProps, createMockStore()(state));
         expect(wrapper.find(Route).props().render({location: ''})).toEqual(<Redirect to={expect.anything()} />);
+    });
+
+    it('should authenticate the user when is not logged and jwt cookie is set', () => {
+        const state: ApplicationState = {
+            ...initialAppState,
+            user: {
+                login: {
+                    logged: false,
+                    loading: false,
+                }
+            }
+        };
+        const props: AuthedRouteProps = { ...routeProps, cookies: new Cookies() };
+        props.cookies.set(Cookie.AUTH, 'token');
+
+        const store = createMockStore()(state);
+        store.dispatch = jest.fn();
+
+        connectedWrapper(props, store);
+        expect(store.dispatch).toBeCalledWith(userActionners.loginJwtRequest());
     });
 });
